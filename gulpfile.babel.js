cdcf900fc17588a4c959f09dffc3422b52f77806
chamo <sherry60421@gmail.com>
2021-10-31 00:53:20 +0800

    [feature]退休倒數

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -10,87 +10,89 @@ import source from 'vinyl-source-stream';
 import buffer from 'vinyl-buffer';
 import sourcemaps from 'gulp-sourcemaps';
 import gutil from 'gulp-util';
-import image from 'gulp-image';
+import imagemin from 'gulp-imagemin';
 
 
 const dirs = {
-  src: './src',
-  dest: './dist'
+    src: './src',
+    dest: './dist'
 };
 
 const pagesPaths = {
-  src: `${dirs.src}/*.html`,
-  dest: `${dirs.dest}`
+    src: `${dirs.src}/*`,
+    dest: `${dirs.dest}`
 };
 
 const stylesPaths = {
-  src: `${dirs.src}/styles/*.scss`,
-  dest: `${dirs.dest}/css`
+    src: `${dirs.src}/styles/*.scss`,
+    dest: `${dirs.dest}/css`
 };
 
 const scriptsPaths = {
-  src: `${dirs.src}/javascripts/*.js`,
-  dest: `${dirs.dest}/js`
+    src: `${dirs.src}/javascripts/*.js`,
+    dest: `${dirs.dest}/js`
 };
 
 const imagesPaths = {
-  src: `${dirs.src}/images/*`,
-  dest: `${dirs.dest}/img`
+    src: `${dirs.src}/images/*`,
+    dest: `${dirs.dest}/img`
 };
 
 // 編譯 Scss 任務，完成後送到 dist/css/main.css
 gulp.task('styles', () => {
-  gulp.src(stylesPaths.src)
-    .pipe(sass())         // 編譯 Scss
-    .pipe(gulp.dest(stylesPaths.dest))
-    .pipe(connect.reload());
+    gulp.src(stylesPaths.src)
+        .pipe(sass()) // 編譯 Scss
+        .pipe(gulp.dest(stylesPaths.dest))
+        .pipe(connect.reload());
 });
 
 // 編譯 JavaScript 轉譯、合併、壓縮任務，完成後送到 dist/js/bundle.js
-gulp.task('javascripts', function(){
+gulp.task('javascripts', function() {
     return browserify({
-        entries: ['./src/javascripts/main.js']
-    })
-    .transform(babelify) // 轉譯
-    .bundle()
-    .pipe(source('bundle.js'))
-    .pipe(buffer()) // 從 streaming 轉回 buffered vinyl 檔案
-    .pipe(sourcemaps.init({loadMaps: true})) // 由於我們壓縮了檔案，要用 sourcemaps 來對應原始文件方便除錯
+            entries: ['./src/javascripts/main.js']
+        })
+        .transform(babelify) // 轉譯
+        .bundle()
+        .pipe(source('bundle.js'))
+        .pipe(buffer()) // 從 streaming 轉回 buffered vinyl 檔案
+        .pipe(sourcemaps.init({
+            loadMaps: true
+        })) // 由於我們壓縮了檔案，要用 sourcemaps 來對應原始文件方便除錯
         .pipe(uglify()) // 壓縮檔案
         .on('error', gutil.log)
-    .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest(scriptsPaths.dest));
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest(scriptsPaths.dest));
 });
 
 // 複製 images 任務，完成後送到 dist/images
 gulp.task('images', function() {
-  gulp.src(imagesPaths.src)
-    .pipe(image())
-    .pipe(gulp.dest(imagesPaths.dest));
+    gulp.src(imagesPaths.src)
+        .pipe(imagemin())
+        .pipe(gulp.dest(imagesPaths.dest));
 });
 
 
-gulp.task('html', function () {
-  gulp.src(pagesPaths.src)
-    .pipe(gulp.dest(pagesPaths.dest))
-    .pipe(connect.reload());
+gulp.task('html', function() {
+    gulp.src(pagesPaths.src)
+        .pipe(gulp.dest(pagesPaths.dest))
+        .pipe(connect.reload());
 });
 
 // 啟動測試用 server，root 為 index.html 放置位置
-gulp.task('server', function () {
-  connect.server({
-    root: 'dist',
-    livereload: true,
-    port: 7777
-  });
+gulp.task('server', function() {
+    connect.server({
+        root: 'dist',
+        livereload: true,
+        port: 7788
+    });
 });
 
 // 監聽是否有檔案更新
-gulp.task('watch', function () {
-  gulp.watch(stylesPaths.src, ['styles']);
-  gulp.watch(scriptsPaths.src, ['javascripts']);
-  gulp.watch(imagesPaths.src, ['images']);
-  gulp.watch(pagesPaths.src, ['html']);
+gulp.task('watch', function() {
+    gulp.watch(stylesPaths.src, ['styles']);
+    gulp.watch(scriptsPaths.src, ['javascripts']);
+    gulp.watch(imagesPaths.src, ['images']);
+    gulp.watch(pagesPaths.src, ['html']);
 });
 
 // 兩種任務類型，第一種會啟動 server
--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,61 +1,90 @@
 
-$(function(){
-  // ---- 下班倒數計時器 ---- //
-  // 上班時間按鈕
-  $("#off-button-start button").on('click', function(e){
-    var date = new Date();
+
+var hideEmpNo = function(empNo) {
+    return empNo.substring(0, 2) + "X" + empNo.substring(3, 5) + "X";
+}
+
+$(function() {
     var now = new Date();
-    var $clickedBtn = $(this);
-    if($clickedBtn.val() === "07:30"){
-      date.setHours(16, 30, 0);
-    } else if($clickedBtn.val() === "08:00"){
-      date.setHours(17, 0, 0);
-    } else if($clickedBtn.val() === "08:30"){
-      date.setHours(17, 30, 0);
-    } else{
-      return;
-    }
-    var diff = (date.getTime()/1000) - (now.getTime()/1000);
-    if(diff < 0){
-      $("#off-countdown").FlipClock(-diff);
-      $("#off-after-message").text("你已經加班了");
-    } else{
-      $("#off-countdown").FlipClock(diff, {
-        countdown: true
-      });
-      $("#off-after-message").text("距離下班還剩下");
-    }
-    $("#off-after-message").show();
-    $("#off-countdown").show();
-    // toggle
-    $("#off-button-start button").each(function(index){
-      if($(this).val() === $clickedBtn.val()){
-        $(this).removeClass("btn-default").addClass(".active").addClass("btn-primary");
-      } else{
-        $(this).addClass("btn-default").removeClass(".active").removeClass("btn-primary");
-      }
+    var date = new Date();
+    // ---- 下班倒數計時器 ---- //
+    // 上班時間按鈕
+    $("#off-button-start button").on('click', function(e) {
+        var $clickedBtn = $(this);
+        if ($clickedBtn.val() === "07:30") {
+            date.setHours(16, 30, 0);
+        } else if ($clickedBtn.val() === "08:00") {
+            date.setHours(17, 0, 0);
+        } else if ($clickedBtn.val() === "08:30") {
+            date.setHours(17, 30, 0);
+        } else {
+            return;
+        }
+        var diff = (date.getTime() / 1000) - (now.getTime() / 1000);
+        if (diff < 0) {
+            $("#off-countdown").FlipClock(-diff);
+            $("#off-after-message").text("你已經加班了");
+        } else {
+            $("#off-countdown").FlipClock(diff, {
+                countdown: true
+            });
+            $("#off-after-message").text("距離下班還剩下");
+        }
+        $("#off-after-message").show();
+        $("#off-countdown").show();
+        // toggle
+        $("#off-button-start button").each(function(index) {
+            if ($(this).val() === $clickedBtn.val()) {
+                $(this).removeClass("btn-default").addClass(".active").addClass("btn-primary");
+            } else {
+                $(this).addClass("btn-default").removeClass(".active").removeClass("btn-primary");
+            }
+        });
+        //紀錄下來
+        localStorage.setItem('startTime', $clickedBtn.val());
     });
-    //紀錄下來
-    localStorage.setItem('startTime', $clickedBtn.val());
-  });
 
-  //上次的時間
-  if(localStorage.getItem('startTime')){
-    $("#off-button-start button[value='" + localStorage.getItem('startTime') + "']").click();
-  }
+    //上次的時間
+    if (localStorage.getItem('startTime')) {
+        $("#off-button-start button[value='" + localStorage.getItem('startTime') + "']").click();
+    }
 
-  // ---- 午餐倒數計時器 ---- //
-  var now = new Date();
-  var date = new Date();
-  date.setHours(11, 30, 0);
-  var diff = (date.getTime()/1000) - (now.getTime()/1000);
-  if(diff < 0){
-    $("#lunch-countdown").FlipClock(-diff);
-    $("#lunch-message").text("你已經吃飽了");
-  } else{
-    $("#lunch-countdown").FlipClock(diff, {
-      countdown: true
+    // ---- 午餐倒數計時器 ---- //
+    date.setHours(11, 30, 0);
+    var diff = (date.getTime() / 1000) - (now.getTime() / 1000);
+    if (diff < 0) {
+        $("#lunch-countdown").FlipClock(-diff);
+        $("#lunch-message").text("你已經吃飽了");
+    } else {
+        $("#lunch-countdown").FlipClock(diff, {
+            countdown: true
+        });
+    }
+    // ---- 退休倒數計時器 ---- //
+    $("#birth-start").on("change", function(e){
+        var birth = e.currentTarget.value;
+        var retireDate = new Date(Number(birth.substring(0, 4)) + 65, birth.substring(5, 7), birth.substring(8, 10));
+        var diffRetire = (retireDate.getTime() / 1000) - (now.getTime() / 1000);
+        if(diffRetire < 0){
+          $("#retire-countdown").FlipClock(-diffRetire,{
+            clockFace: 'DailyCounter'
+          });
+          $("#retire-after-message").text("你已經退休了");
+        } else{
+          $("#retire-after-message").text("距離退休還剩下");
+          $("#retire-countdown").FlipClock(diffRetire, {
+              clockFace: 'DailyCounter',
+              countdown: true
+          });
+        }
+        $("#retire-after-message").show();
+        $("#retire-countdown").show();
+        // 記錄下來
+        localStorage.setItem('birth', birth);
     });
-  }
-
+    //上次的時間
+    if (localStorage.getItem('birth')) {
+        $("#birth-start").val(localStorage.getItem('birth'));
+        $("#birth-start").trigger("change");
+    }
 });
